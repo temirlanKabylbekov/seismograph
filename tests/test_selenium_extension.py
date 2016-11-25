cfd684b5c119e39f7e2e24e5274fc83b532291ad
temirlanKabylbekov <temirlan.kabylbekov777@gmail.com>
2016-11-25 20:57:10 +0300

    Add tests for creating remote browser.

--- tests/test_selenium_extension.py
+++ tests/test_selenium_extension.py
@@ -8,7 +8,7 @@ from selenium.common.exceptions import WebDriverException
 from mock import Mock, MagicMock
 from mock import patch
 
-from seismograph.ext.selenium.extension import Selenium
+from seismograph.ext.selenium.extension import Selenium, DEFAULT_BROWSER, DRIVER_TO_CAPABILITIES
 from .lib.factories import selenium_config_factory
 from seismograph.ext.selenium import drivers
 import seismograph.ext.selenium.browser as browser
@@ -30,6 +30,7 @@ class SeleniumTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
+    # tests for `firefox` method
     def test_firefox_with_empty_firefox_config(self):
         """Test `firefox` method.
 
@@ -111,6 +112,103 @@ class SeleniumTestCase(unittest.TestCase):
         s = Selenium(selenium_config)
         browser = browser_create_mock.return_value
         self.assertEqual(browser, s.firefox())
+
+    # tests for `remote` method
+    def test_remote_with_empty_remote_config(self):
+        """Test `remote` method.
+
+        Note:
+            Test to throw exception when remote browser config in
+            general selenium config exists but is empty.
+
+        """
+        selenium_config = {'REMOTE': {}}
+        mock_config = MagicMock(name='selenium_config')
+        mock_config.get.side_effect = selenium_config.get
+
+        mock_driver = MagicMock(name='driver_name')
+
+        s = Selenium(mock_config)
+        with self.assertRaises(SeleniumExError) as context:
+            s.remote(mock_driver)
+        self.assertTrue(
+            'settings for remote is not found in config' in context.exception
+        )
+
+    @patch('seismograph.ext.selenium.browser.create')
+    @patch('seismograph.ext.selenium.drivers.RemoteWebDriver')
+    def test_remote_set_desired_capabilities_config(
+            self, webdriver_create_mock, browser_create_mock):
+        """Test `remote` method.
+
+        Note:
+            Test setting `desired_capabilities` in remote browser config with
+            this empty property.
+
+        """
+        selenium_config = {'REMOTE': {'desired_capabilities': {}}}
+        mock_config = MagicMock(name='selenium_config')
+        mock_config.get.side_effect = selenium_config.get
+
+        s = Selenium(mock_config)
+        s.remote(DEFAULT_BROWSER)
+        self.assertEqual(
+            mock_config.get('REMOTE').get('desired_capabilities'),
+            DRIVER_TO_CAPABILITIES[DEFAULT_BROWSER]
+        )
+
+    @patch('seismograph.ext.selenium.browser.create')
+    @patch('seismograph.ext.selenium.drivers.RemoteWebDriver')
+    def test_remote_set_desired_capabilities_config(
+            self, webdriver_create_mock, browser_create_mock):
+        """Test `remote` method.
+
+        Note:
+            Test removing `capabilities` after setting `desired_capabilities` in 
+            remote browser config with this empty property.
+
+        """
+        selenium_config = {
+            'REMOTE': {
+                'desired_capabilities': {},
+                'capabilities': {
+                    'driver_name': DEFAULT_BROWSER
+                }
+            }
+        }
+        mock_config = MagicMock(name='selenium_config')
+        mock_config.get.side_effect = selenium_config.get
+        mock_config.__contains__.side_effect = selenium_config.__contains__
+
+        s = Selenium(mock_config)
+        s.remote(DEFAULT_BROWSER)
+
+        self.assertTrue(
+            'capabilities' not in mock_config.get('REMOTE')
+        )
+
+    def test_remote_webdriver_init(self):
+        """Test `remote` method.
+
+        Note:
+            Test to create web driver by given remote browser config.
+
+        """
+        selenium_config = {
+            'REMOTE': {
+                'desired_capabilities': DRIVER_TO_CAPABILITIES[DEFAULT_BROWSER],
+            }
+        }
+        mock_config = MagicMock()
+        mock_config.get.side_effect = selenium_config.get
+
+        with patch('seismograph.ext.selenium.browser.create') as browser_create_mock:
+            browser_create_mock.return_value(None)
+            s = Selenium(mock_config)
+            try:
+                s.remote(DEFAULT_BROWSER)
+            except WebDriverException:
+                self.fail('remote driver fails to create')
 
 
 def main():
