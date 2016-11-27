0a533dcf5717355c5057c13d3c919c6356163c62
rytovkopat <gzintzfirmltd@mail.ru>
2016-11-28 00:02:32 +0300

    Add tests for `utils.py`.

---
+++ tests/test_selenium_utils.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from mock import MagicMock, patch
+
+from seismograph.ext.selenium.utils import random_file_name, \
+    change_name_from_python_to_html, declare_standard_callback, \
+    is_ready_state_complete, re_raise_exc
+from seismograph.ext.selenium.proxy import WebDriverProxy
+from seismograph.ext.selenium.exceptions import ReRaiseException
+
+
+class UtilsTestCase(unittest.TestCase):
+    """Tests for `utils.py` module.
+
+    """
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @patch('random.randint', return_value=500)
+    @patch('time.time', return_value=100500.1005)
+    def test_random_file_name_check_algorithm(self, mock_time, mock_randint):
+        """Test `random_file_name` function.
+
+        Note:
+            Test algorithm to generate file name.
+
+        """
+        # TO CODE REVIEWER: не получилось замокать random.randint. What is the problem?
+        self.assertTrue(100500 + 0 <= int(random_file_name()) <= 100500 + 1000)
+
+    def test_change_name_from_python_to_html_startswith_underscore(self):
+        """Test `change_name_from_python_to_html` function.
+
+        Note:
+            Test when passing `name` startswith `_`.
+
+        """
+        python_name = '_python_name'
+        html_name = change_name_from_python_to_html(python_name)
+        self.assertEqual(html_name, 'python-name')
+
+    def test_change_name_from_python_to_html_endswith_underscore(self):
+        """Test `change_name_from_python_to_html` function.
+
+        Note:
+            Test when passing `name` endswith `_`.
+
+        """
+        python_name = 'python_name_'
+        html_name = change_name_from_python_to_html(python_name)
+        self.assertEqual(html_name, 'python-name')
+
+    def test_change_name_from_python_to_html_contains_underscore(self):
+        """Test `change_name_from_python_to_html` function.
+
+        Note:
+            Test when passing `name` contains `_` in the middle of part.
+
+        """
+        python_name = 'python_name'
+        html_name = change_name_from_python_to_html(python_name)
+        self.assertEqual(html_name, 'python-name')
+
+    def test_is_ready_state_complete_check_passing_execute_script_arg(self):
+        """Test `is_ready_state_complete` function.
+
+        Note:
+            Test the argument passing for `execute_script` is
+            `return document.readyState`.
+
+        """
+        browser = MagicMock(spec=WebDriverProxy)
+        is_ready_state_complete(browser)
+        browser.execute_script.assert_called_with('return document.readyState')
+
+    def test_declare_standard_callback_pass_not_callable(self):
+        """Test `declare_standard_callback` function.
+
+        Note:
+            Test when passing not callable arg.
+
+        """
+        not_callable_object = 123456
+        self.assertEqual(
+            declare_standard_callback(not_callable_object),
+            not_callable_object
+        )
+
+    def test_declare_standard_callback_pass_python_builtin_function(self):
+        """Test `declare_standard_callback` function.
+
+        Note:
+            Test when passing python builtin function.
+
+        """
+        python_builtin_function = len
+        self.assertEqual(
+            declare_standard_callback(python_builtin_function),
+            python_builtin_function
+        )
+
+    def test_declare_standard_callback_pass_function_with_many_args(self):
+        """Test `declare_standard_callback` function.
+
+        Note:
+            Test when passing function which has more than one arg in signature.
+
+        """
+        def stub_foo(x, y):
+            pass
+
+        with self.assertRaises(TypeError) as context:
+            declare_standard_callback(stub_foo)
+            self.assertTrue('Incorrect signature of function' in context.exception)
+
+    def test_declare_standard_callback_pass_function_with_one_arg(self):
+        """Test `declare_standard_callback` function.
+
+        Note:
+            Test when passing function which has just one arg in signature.
+
+        """
+        def stub_foo(x):
+            pass
+
+        self.assertEqual(
+            declare_standard_callback(stub_foo),
+            stub_foo
+        )
+
+
+class ReRaiseRxcTestCase(unittest.TestCase):
+    """Tests for `re_raise_exc` function in 'utils.py' module.
+
+    """
+    def setUp(self):
+        self.base_exception_message = 'base message'
+        self.mock_foo = MagicMock()
+        self.mock_foo.__name__ = 'name'
+        self.mock_foo.side_effect = BaseException(self.base_exception_message)
+
+    def tearDown(self):
+        pass
+
+    def test_re_raise_exc_not_specify_message(self):
+        """Test `re_raise_exc` function.
+
+        Note:
+            Test throwing exception.
+
+        """
+        try:
+            wrapped = re_raise_exc(self.mock_foo)
+            wrapped()
+        except ReRaiseException as e:
+            self.assertEqual(e.message, self.base_exception_message)
+
+    def test_re_raise_exc_specify_message(self):
+        """Test `re_raise_exc` function.
+
+        Note:
+            Test throwing exception with specified
+            message.
+
+        """
+        re_raise_exception_message = 're-raise message'
+        expected_message = u'{}{}'.format(
+            re_raise_exception_message,
+            u' (from BaseException: {})'.format(
+                self.base_exception_message
+                if re_raise_exception_message else
+                '(from BaseException)'
+            )
+        )
+        try:
+            wrapped = re_raise_exc(self.mock_foo, message=re_raise_exception_message)
+            wrapped()
+        except ReRaiseException as e:
+            self.assertEqual(e.message, expected_message)
+
+    def test_re_raise_exc_specify_exception(self):
+        """Test `re_raise_exc` function.
+
+        Note:
+            Test throwing special type of exception
+            passing to this function.
+
+        """
+        try:
+            wrapped = re_raise_exc(self.mock_foo, exc_cls=ValueError)
+            wrapped()
+        except ValueError as e:
+            self.assertEqual(e.message, self.base_exception_message)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
